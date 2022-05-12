7e1d0548224b3665b3552c6821b0ff4dedb4064c
algre_din@student.asdah.school.nz <algre_din@student.asdah.school.nz>
2022-05-12 14:42:23 +1200

    Added some print

--- main/algre-quiz.py
+++ main/algre-quiz.py
@@ -2,18 +2,18 @@ from operator import truediv
 from Question import Question
 
 question_prompts = [
-    "1. What is Larry's superhero name?\nA Minnesota Cuke\nB Larryboy\nC Supercuke\nD Green Larry\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
+    "1. What is Larry's superhero name?\nA Minnesota Cuke\nB Larryboy\nC Supercuke\nD Green Larry\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
     "2. What bible story does Veggietales portray in Duke and the Great Pie War episode?\nA Ruth and Boaz\nB Rahab and the spies\nC David and Goliath\nD Adam and Eve\n\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "3. What is the name of the computer?\nA Asdf\nB Qwerty\nC Uiop\nD Ghjkl\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "4. What type of vegetable is Petunia?\nA Radish\nB Broccoli\nC Rhubarb\nD Asparagus\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "5. Who sang the song ‘Pizza Angel’?\nA Bob\nB Larry\nC Petunia\nD Pa Grape\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "6. Who sang the song ‘Lance the Turtle’?\nA Bob\nB Larry\nC Petunia\nD Archibald\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
+    "3. What is the name of the computer?\nA Asdf\nB Qwerty\nC Uiop\nD Ghjkl\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
+    "4. What type of vegetable is Petunia?\nA Radish\nB Broccoli\nC Rhubarb\nD Asparagus\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
+    "5. Who sang the song ‘Pizza Angel’?\nA Bob\nB Larry\nC Petunia\nD Pa Grape\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
+    "6. Who sang the song ‘Lance the Turtle’?\nA Bob\nB Larry\nC Petunia\nD Archibald\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
     "7. Finish the sentence: “We are the pirates...”\nA who don’t do anything.\nB who sail the seven seas.\nC who are searching for the whale.\nD who crashed their ship.\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "8. Who is the Supper Hero?\nA Archibald\nB Larry\nC Jerry\nD Jimmy\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "9. Who played Jonah in Jonah: A Veggietales Movie?\nA Jerry\nB Bob\nC Pa Grape\nD Archibald\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
-    "10. Who is the very model of a modern major general?\nA Jerry\nB Bob\nC Pa Grape\nD Archibald\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
+    "8. Who is the Supper Hero?\nA Archibald\nB Larry\nC Jerry\nD Jimmy\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
+    "9. Who played Jonah in Jonah: A Veggietales Movie?\nA Jerry\nB Bob\nC Pa Grape\nD Archibald\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
+    "10. Who is the very model of a modern major general?\nA Jerry\nB Bob\nC Pa Grape\nD Archibald\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
     "11. Who played the angel in Gideon the Tuba Warrior?\nA Petunia\nB Pa Grape\nC Madame Blueberry\nD Archibald\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
-    "12. How did Toto (Junior) save the land of Woe in Lord of the Beans?\nA He tosses the bean into the dry well.\nB He fights off all the sporks.\nC He sells grape soda to all the people in town.\nD He wishes for eternal riches and gives a portion to each veggie in the land.\nPLEASE TYPE JUST THE LETTER  e.g. A >>",
+    "12. How did Toto (Junior) save the land of Woe in Lord of the Beans?\nA He tosses the bean into the dry well.\nB He fights off all the sporks.\nC He sells grape soda to all the people in town.\nD He wishes for eternal riches and gives a portion to each veggie in the land.\nPLEASE TYPE JUST THE LETTER IN CAPS e.g. A >>",
 ]
 
 questions = [
